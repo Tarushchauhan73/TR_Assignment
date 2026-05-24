aa695a2a49dc5aa99dbb2eafb920c54d7a569fac
Tarush chauhan <156651501+Tarushchauhan73@users.noreply.github.com>
2026-05-24 07:50:31 +0000

    fix playwright and testrelic integration

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,12 +4,21 @@ export default defineConfig({
   testDir: './tests',
 
   reporter: [
+    ['list'],
     ['html'],
     ['json', { outputFile: 'playwright-report.json' }],
-    ['list']
+    [
+      '@testrelic/playwright-analytics',
+      {
+        cloud: {
+          apiKey: 'tr_live_d0428ea55f79a1ada426ff755957d9c48918f1b6570fcba510775f079ef2f49e',
+        },
+      },
+    ],
   ],
 
   use: {
     trace: 'on-first-retry',
+    headless: true,
   },
 });
---
+++ tests/example.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+test('homepage test', async ({ page }) => {
+  await page.goto('https://example.com');
+
+  await expect(page).toHaveTitle(/Example/);
+});
