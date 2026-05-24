42013c7f693a8b46dc03e32c53468446a707dd42
Tarush chauhan <156651501+Tarushchauhan73@users.noreply.github.com>
2026-05-24 07:40:18 +0000

    fix playwright github actions

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,6 +3,12 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
 
+  reporter: [
+    ['html'],
+    ['json', { outputFile: 'playwright-report.json' }],
+    ['list']
+  ],
+
   use: {
     trace: 'on-first-retry',
   },
