da3c869b4765069296289496ec958cdb6f5bac7d
Tarush chauhan <156651501+Tarushchauhan73@users.noreply.github.com>
2026-05-24 08:01:52 +0000

    finalize testrelic integration

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,14 +11,9 @@ export default defineConfig({
       '@testrelic/playwright-analytics',
       {
         cloud: {
-          apiKey: 'tr_live_d0428ea55f79a1ada426ff755957d9c48918f1b6570fcba510775f079ef2f49e',
+          apiKey: process.env.TESTRELIC_API_KEY,
         },
       },
     ],
   ],
-
-  use: {
-    trace: 'on-first-retry',
-    headless: true,
-  },
 });
